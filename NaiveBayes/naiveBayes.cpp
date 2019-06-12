16511a810c04e92d45d06860742140caf2a724ed
learnermaxRL <sharma.mayankproacc@gmail.com>
2019-06-13 02:35:00 +0530

    Pushing basic Naive Bayes algo

---
+++ NaiveBayes/naiveBayes.cpp
@@ -0,0 +1,94 @@
+#include "naive_bayes.h"
+#include<iostream>
+#include<wchar.h>
+
+
+
+void NaiveBayes::AddData(std::string cl,std::vector<int> dataF){
+
+    data d;
+    d.label = NaiveBayes::labelmap[cl];
+    d.val = dataF;
+    NaiveBayes::trainData.push_back(d);
+}
+
+void NaiveBayes::calculateProbs(){
+
+    for (int i=0; i < trainData.size();i++){
+
+        classProbMap[trainData[i].label]++;
+        std::vector<int> featCounter(featureVecsize,0);
+        for (int j=0;j< NaiveBayes::featureVecsize; j++){
+             if (trainData[i].val[j] !=0) {
+             *( classProbFeaturewise[trainData[i].label].begin() + j)++;}
+        }
+    }
+    std::map<int, double>::iterator it = classProbMap.begin();
+    
+    while (it!=classProbMap.end()){
+        it->second = it->second/trainData.size();
+        std::vector<double>::iterator it_feat = classProbFeaturewise[it->second].begin();
+            while (it_feat!= classProbFeaturewise[it->second].end()){
+
+                *it_feat = *it_feat/trainData.size();
+                it_feat++;
+            }
+
+
+    }
+
+}
+void NaiveBayes::train(){
+    NaiveBayes::featureVecsize = trainData[0].val.size();
+    calculateProbs();
+
+}
+std::vector<double> NaiveBayes::Predict(std::vector<int> da){
+
+    std::vector<double> probVec;
+    double p;
+    for (int x = 0; x < labelmap.size();x++){
+        p = classProbMap[x];
+        for (int j = 0 ; j < da.size(); j++){
+            if (da[j] !=0 ){
+                p = p * classProbFeaturewise[x][j];
+            }
+        probVec.push_back(p);
+
+
+        }
+    }
+    return probVec;
+
+}
+
+
+int main (){
+
+
+    NaiveBayes nb;
+    nb.AddData("first",{0,0,1,1,0,0,1});
+    nb.AddData("second",{0,0,1,1,0,0,1});
+    nb.AddData("third",{0,0,1,1,0,0,1});
+    nb.AddData("fourth",{0,0,1,1,1,0,1});
+    nb.AddData("third",{0,0,1,1,0,0,1});
+    nb.AddData("fourth",{1,0,1,1,0,0,1});
+    nb.AddData("first",{1,0,1,1,0,0,1});
+    nb.AddData("second",{0,0,1,0,1,1,1});
+    nb.AddData("fourth",{0,0,1,0,1,1,1});
+    nb.AddData("second",{0,0,1,0,1,1,1});
+    nb.AddData("third",{1,0,1,0,1,1,1});
+    nb.AddData("first",{1,1,1,0,1,1,1});
+    nb.train();
+    std::vector<double> res =  nb.Predict({0,0,0,0,1,0,1});
+    std::vector<double>::iterator it = res.begin();
+    while (it!=res.end()){
+        std::cout<< *it<<"\n";
+
+    }
+
+   return 0;
+
+}
+
+
---
+++ NaiveBayes/naive_bayes.h
@@ -0,0 +1,40 @@
+/* Given a finite set of labels the algorithm assigns labels to some input data,this algo assumes that   */
+#include <map>
+#include<vector>
+#include<string>
+
+
+
+struct data{
+
+    int label;
+    std::vector<int> val;
+
+
+};
+
+class NaiveBayes {
+
+    private:
+        int numClasses;
+        std::vector<data> trainData;
+        int featureVecsize;
+        std::map<std::string,int> labelmap ;
+        std::map<int,double> classProbMap;
+        std::map<int,std::vector<double> > classProbFeaturewise;
+
+    
+    public:
+        NaiveBayes(){
+
+    labelmap.insert({"first",0});
+    labelmap.insert({"second",1});
+    labelmap.insert({"third",2});
+
+};
+         
+         void calculateProbs();
+         void train();
+         std::vector<double>  Predict(std::vector<int>);
+         void AddData(std::string ,std::vector<int> );
+};
